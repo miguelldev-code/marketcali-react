dd438ac6f22a8c6c52e30651eb1bf339b77ec230
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-17 19:43:55 -0500

    Importaciones necesarias css y configuracion predeterminada

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./css/App.css";
+import "./css/base.css";
+import "./css/HomePage.css";
+import "./css/Navbar.css";
+import "./css/Footer.css";
+import "./css/HomePage.css";
+import "./css/About.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
